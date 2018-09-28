8171823bf8899a82608428f9e5e5a85c6c135965
winand appels <winnie33winnie33@gmail.com>
2018-09-28 10:38:08 +0200

    some kind of chat? Absolutely horrible though

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client', express.static(__dirname+ '/client'));
 
-serv.listen(3000);
+io.on('connection', function(socket){
+  console.log('Someone connected!');
+
+  socket.on('input', function(text){
+    console.log('Input received! Message: ' + text);
+    // Send the message back to everyone connected
+    io.emit('response', text);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('Aww, someone disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('Server is up, listening on *:3000');
+});
